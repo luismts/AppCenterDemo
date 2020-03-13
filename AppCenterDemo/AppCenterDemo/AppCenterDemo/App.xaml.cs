7af1bc1106b17b5a77ffef3a8d0dd1195bb12023
luismts <deivi_0326@hotmail.com>
2020-03-12 23:19:19 -0400

    Tests added

--- AppCenterDemo/AppCenterDemo/AppCenterDemo/App.xaml.cs
+++ AppCenterDemo/AppCenterDemo/AppCenterDemo/App.xaml.cs
@@ -2,6 +2,10 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace AppCenterDemo
 {
     public partial class App : Application
@@ -19,6 +23,10 @@ namespace AppCenterDemo
 
         protected override void OnSleep()
         {
+            AppCenter.Start("android=77b6d313-bc2c-40ae-95ee-9320689a7c47;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnResume()
--- AppCenterDemo/AppCenterDemo/AppCenterDemo/MainPage.xaml.cs
+++ AppCenterDemo/AppCenterDemo/AppCenterDemo/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -16,6 +18,67 @@ namespace AppCenterDemo
         public MainPage()
         {
             InitializeComponent();
+            Analytics.TrackEvent("Getting started!");
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // Note: checks for debug vs release configuration.
+            // You can only use it when debugging as it won't work for release apps.
+            Crashes.GenerateTestCrash(); 
+        }
+        private void Button_Clicked_Error1(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception exception)
+            {
+                Crashes.TrackError(exception);
+            }
+        }
+
+        private void Button_Clicked_Error2(object sender, EventArgs e)
+        {
+            int[] myNumbers = { 1, 2, 3 };
+
+            try
+            {
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception exception)
+            {
+                // You can also optionally add one binary (Ex. images)
+                // and one text attachment to a handled error report. 
+                var properties = new Dictionary<string, string> {
+                    { "Note", "Array problem" },
+                    { "myNumbers", myNumbers.ToString() }
+                  };
+
+                Crashes.TrackError(exception, properties);
+            }
+        }
+
+        private void Button_Clicked_Event(object sender, EventArgs e)
+        {
+            Analytics.TrackEvent("Button Event clicked");
+        }
+
+        int counter = 0;
+        private void Button_Clicked_Event2(object sender, EventArgs e)
+        {
+            Analytics.TrackEvent("Button Event 2 clicked", new Dictionary<string, string> {
+                { "Page", "MainPage" },
+                { "CounterClick", counter++.ToString()}
+            });
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            int[] myNumbers = { 1, 2, 3 };
+            Console.WriteLine(myNumbers[10]); // Crash the app :(
         }
     }
 }
